32b37b540fdb4597e7d7ebf07c0c204ed69f8157
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-25 17:12:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.length >= 1
+    katz_deli.length != 0
     katz_deli.each do |value, index|
    puts  "The line is currently: #{index}. #{value}"
     end
