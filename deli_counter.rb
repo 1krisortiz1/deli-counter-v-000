74bbca55415f2e9e0c04be89bbece8ce0b392692
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-25 17:26:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else
     katz_deli.length != 0
     katz_deli.each_with_index do |value, index|
-      puts  "The line is currently: #{index+1}. #{value}"
+      puts "The line is currently: #{index+1}. #{value}"
     end
   #  index + 1
   end
