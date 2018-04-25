c96a90e1dcb4162c4f82d4c8bd2c98e51816073f
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-25 18:03:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,8 @@ def line(katz_deli)
     katz_deli.length != 0
     katz_deli.each_with_index(0) do |value, index|
       katz_deli.push("#{index}. #{value}")
-      puts "The line is currently: #{katz_deli.join(" ")}  #{#{index+1}. #{value}"
+      puts "The line is currently: #{katz_deli.join(" ")}  
+    
     end
   end
 end
