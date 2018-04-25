7adda0f2174c5030ffec8732c40a87a09326c1cd
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-25 17:22:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.length != 0
-    katz_deli.each_with_index do {|value, index| puts  "The line is currently: #{index+1}. #{value}"}
+    katz_deli.each_with_index do {|value, index| +1 }
+      puts  "The line is currently: #{index+1}. #{value}"
     end
   #  index + 1
   end
